5b81a1e1a35bf30a31d460bc66eeb662a9a9f283
im-tearain <im.tearain@gmail.com>
2019-04-10 14:39:08 +0200

    Update TipoDocumento

--- src/main/java/es/fpdual/primero/eadmin/modelo/TipoDocumento.java
+++ src/main/java/es/fpdual/primero/eadmin/modelo/TipoDocumento.java
@@ -3,11 +3,20 @@ package es.fpdual.primero.eadmin.modelo;
 public enum TipoDocumento {
 
 	//Clase estática
-	DOCUMENTO_CONTABLE, 
-	DOCUMENTO_FACTURA, 
-	DOCUMENTO_NOMINA, 
-	DOCUMENTO_SUBVENCION, 
-	DOCUMENTO_PADRON;
+	DOCUMENTO_CONTABLE(0), 
+	DOCUMENTO_FACTURA(1), 
+	DOCUMENTO_NOMINA(2), 
+	DOCUMENTO_SUBVENCION(3), 
+	DOCUMENTO_PADRON(4);
 	
+	private final int id;
+
+	private TipoDocumento(int id) {
+		this.id = id;
+	}
+
+	public int getId() {
+		return id;
+	}
 	
 }
